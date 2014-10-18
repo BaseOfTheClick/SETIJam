b30ca52c2c51954f4a2fdc674673a554f2f39725
Jesse Hamburger <jesselhamburger@gmail.com>
2014-10-18 02:08:50 -0700

    Added Assets

---
+++ Assets/Scripts/MaskCutoff.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaskCutoff : MonoBehaviour {
+
+	public float cutoff;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		renderer.material.SetFloat("_Cutoff", Mathf.InverseLerp(0, Screen.width, cutoff));
+
+	}
+}
