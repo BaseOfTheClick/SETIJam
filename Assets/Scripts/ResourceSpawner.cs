fff55039560e5174aeb384f4737a651e367ab804
amixtum <ganiparrott@gmail.com>
2014-10-18 16:25:55 -0700

    Implemented rates of change.

---
+++ Assets/Scripts/ResourceSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceSpawner : MonoBehaviour {
+    public GameObject resourcePackage;
+
+    public IntelligentPlanet planet;
+    private Vector3[] planetVertices;
+
+    private float timeInterval = 3f;
+    private float timeAccumulate = 0;
+
+	// Use this for initialization
+	void Start () {
+        planet = this.GetComponent<IntelligentPlanet>();
+        planetVertices = planet.GetComponent<MeshFilter>().mesh.vertices;
+        timeInterval = this.GetComponent<IntelligentPlanet>().rateOfChange;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeAccumulate += Time.deltaTime;
+
+        if (timeAccumulate >= timeInterval)
+        {
+            Vector3 randomVertex = planetVertices[Random.Range(0, planetVertices.Length)];
+            GameObject clone = Instantiate(resourcePackage, randomVertex, Quaternion.identity) as GameObject;
+            clone.transform.parent = planet.gameObject.transform;
+            timeAccumulate = 0;
+        }
+	}
+}
--- Assets/Scripts/SocietyBar.cs
+++ Assets/Scripts/SocietyBar.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 /**
  TODO : Add class description
 */
-public class SocietyBar : MonoBehaviour {
-	//rate of change
-	public float rateOfChange;
-	
-	//discrete value 
-	public int resources;
+public class SocietyBar : MonoBehaviour
+{
+    public float packetSize;
+
+    //rate of change
+    //normalized value : between 0, and 1
+    public float rateOfChange;
+
+    //discrete value 
+    public float resources = 50;
 
     //max resource value
     public int resourcesMax = 100;
@@ -19,68 +23,76 @@ public class SocietyBar : MonoBehaviour {
     //constants for visual rep. of values
     public float minGfx = 68f;
     public float maxGfx = 200f;
-	
-	//default constructor
-	//doesn't set anything
-	public SocietyBar() {}
-	
-	//sets initial values or rateOfChange and resources
-	public SocietyBar(float rateOfChange, int resources) {
-		this.rateOfChange = rateOfChange;
-		this.resources = resources;
-	}
-	
-	public float getRateOfChange() {
-		return rateOfChange;
-	}
-	
-	public int getResources() {
-		return resources;
-	}
-	
-	public void setRateOfChange(float rOC) {
-		this.rateOfChange = rOC;
-	}
-	
-	public void setResources(int res) {
-		this.resources = res;
-	}
-	
-	//shortcut to writing setResources(getResources() + some_num)
-	public void addResources(int val) {
-		this.resources += val;
-	}
+
+    private float timeAccumulate = 0;
+
+    //default constructor
+    //doesn't set anything
+    public SocietyBar() { }
+
+    ////sets initial values or rateOfChange and resources
+    //public SocietyBar(float rateOfChange, int resources)
+    //{
+    //    this.rateOfChange = rateOfChange;
+    //    this.resources = resources;
+    //}
+
+    public void Start()
+    {
+        conveyResources();
+    }
+
+    public float getRateOfChange()
+    {
+        return rateOfChange;
+    }
+
+    public float getResources()
+    {
+        return resources;
+    }
+
+    public void setRateOfChange(float rOC)
+    {
+        this.rateOfChange = rOC;
+    }
+
+    public void setResources(int res)
+    {
+        this.resources = res;
+    }
+
+    //shortcut to writing setResources(getResources() + some_num)
+    public void addResources(int val)
+    {
+        this.resources += val;
+    }
 
     //translate discrete resource quantity to graphical representation
     public void conveyResources()
     {
         //max = 200
         //min = 68
-        GameObject bar = GameObject.Find("SocietyBar_Mask/SocietyBar_Bar");
+        GameObject bar = GameObject.Find("Bar_Mask/Bar");
 
         RectTransform barRect = bar.GetComponent<RectTransform>();
 
-        float diff = (maxGfx - minGfx) * (1 + (resources / resourcesMax));
+        float diff = minGfx + ((resources * (maxGfx - minGfx)) / 100);
+
+        Debug.Log(diff);
 
         barRect.sizeDelta = new Vector2(diff, diff);
     }
-	
-	//updates the resource value based on the rate of change
-	//returns whether the value was less than 0 upon updating resources
-	public bool updateResources() {
-		bool lessThanZero;
-		
-		resources = resources + (int)(resources * rateOfChange);
-		
-		lessThanZero = (resources < 0) ? true : false;
-		
-		resources = (lessThanZero) ? 0 : resources;
-		
-		return lessThanZero;
-	}
 
-    public void update()
+    //updates the resources based on the rate of change
+    public void updateResources()
+    {
+        resources = (resources - packetSize < 0) ? 0 : resources - (float)(packetSize) * Time.deltaTime;
+    }
+
+    public void Update()
     {
         updateResources();
+        conveyResources();
     }
 }
