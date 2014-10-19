bd13faf4de4af51fc1582cdc123bdea7a5e0f758
Jesse Hamburger <jesselhamburger@gmail.com>
2014-10-19 06:53:55 -0700

    stars

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	private LensFlare flare;
+
+	// Use this for initialization
+	void Start () {
+		flare = gameObject.GetComponent<LensFlare>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		flare.brightness -= 1.0f * Time.deltaTime;
+
+		if ( flare.brightness <= 0.0f ) {
+
+			Destroy(gameObject);
+
+		}
+
+	}
+}
